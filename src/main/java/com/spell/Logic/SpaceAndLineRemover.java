a621dff783731e19c53222287d7dc4917bc149ea
Hannah Pulmano <hannah.pulmano865@gmail.com>
2024-05-04 15:39:57 +0800

    LineBreakRemover and ExcessSpaceRemover

--- src/main/java/com/spell/Logic/SpaceAndLineRemover.java
+++ src/main/java/com/spell/Logic/SpaceAndLineRemover.java
@@ -5,11 +5,35 @@ public class SpaceAndLineRemover extends SPELLEditor {
         super(textToEdit);
     }
 
-    public void removeLineBreaks() {
-        // ... 
+    public String removeLineBreaks() {
+        String text = super.excessSpaceRemover();
+        String substring = text;
+        sbEditor.append(text);
+
+
+        while (substring.contains("\n")) {
+            sbEditor.deleteCharAt(text.indexOf("\n"));
+            substring = sbEditor.toString();
+        }
+
+        text = sbEditor.toString();
+        sbEditor.delete(0, sbEditor.length());
+        return text;
     }
 
-    public void removeSpaces() {
-        // ... " "
+    public String removeSpaces() {
+        String text = super.getText();
+        sbEditor.append(text);
+
+        for(int i = 0; i < sbEditor.length(); i++) {
+            if (Character.isWhitespace(sbEditor.charAt(i))) {
+                sbEditor.deleteCharAt(i);
+                i--;
+            }
+        }
+
+        text = sbEditor.toString();
+        sbEditor.delete(0, sbEditor.length());
+        return text;
     }
 }
